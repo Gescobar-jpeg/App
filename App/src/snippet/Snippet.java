5805f78f13333e5b84b88b4f41f0d87397a27c41
Gustavo Escobar <you@example.com>
2020-11-14 22:09:30 -0800

    first commit

---
+++ App/src/snippet/Snippet.java
@@ -0,0 +1,20 @@
+package snippet;
+
+public class Snippet {
+	GitHub is an open source community, a complex share drive used by developers across the globe. It is widely trusted for its flexibility and security and used for both small and large scale projects.  Git is useful because its platform allows problem solvers all around the world to share ideas in real time and improve the world around them. In Git Workflow, share drives are called repositories, these repositories are accessible by all working on the project. Allows someone to pull out the changes that someone else has made and update on those changes. Workflow also allows the user to take screenshots of their own work to later share with teammates. The wonderful thing about this software is that multiple users can work on the same project at once, but when a user updates a project, the previous user’s work is not over written. 
+	
+	The eight primitive types are all meant to hold simple values. Each of the primitive types holds a unique type of value. The eight types are as follow:
+	•	Integers are whole numbers and can be both positive and negative values.  Integers take up 2 bytes of data and can store up to 10 digits of value.
+	•	Float has a decimal place, more commonly used for simple decimals and takes up 4 bytes of data.
+	•	Double is the precision decimal data type of number. This is used for more complex decimal types which range up to 15 digits. This type holds 8 bytes of data.
+	•	Long primitive types are like integers in that they store whole values, however they are not used very often, as they are only required when the integer limit has been reached. This type stores values up to 19 digits both as positive and negative numbers and holds 8 bytes of data.
+	•	Short is also like an integer that contains both positive and negative numbers, but the digit range is much smaller.
+	•	A Byte’s value lives to its name, it holds one byte of data and can store both positive and negative values up to 3 digits.
+	•	Char is meant for holding a single character like “A, B, C” and holds only 2 bytes of data so it can be used for international numbers.
+	•	Boolean are used to store conditional statements such as “true or false” and like a Byte only hold one byte of data. 
+	
+	My favorite thing to have learned this week has to be the about concatenation. The “+” symbol would normally add numeric data but when used with textual data one performs concatenation. Concatenation uses the “+” symbol but because you are adding textual data it combines two Strings together. This feature also allows the user to add spaces into code by simply adding “ “ to the string code. 
+		
+	
+}
+
---
+++ Pikachu/src/module-info.java
@@ -0,0 +1,2 @@
+module pikachu {
+}
